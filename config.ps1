ec9a422d197549fe3fbc543ad9dd79aafa967de7
Temeraire130 <becker-simeon@t-online.de>
2023-09-27 20:18:34 +0200

    Added creation of aird file

--- config.ps1
+++ config.ps1
@@ -1,5 +1,6 @@
 $config = Get-Content -Raw -Path config.json | ConvertFrom-Json
 $projects = $config.projects
+$metamodelpositions = $config.metamodelpositions
 $git = "git"
 
 function ConvertToKeyStrokes {
@@ -15,6 +16,30 @@ function ConvertToKeyStrokes {
     }
     $return += "{TAB}{TAB}{TAB}{ENTER}"
     return $return
+}
+
+function KeyStrokesForMetamodels {
+    param([int[]]$indices)
+    #[System.Windows.Forms.SendKeys]::SendWait("^{F7}{UP}{UP}{UP}{UP}")
+    [System.Windows.Forms.SendKeys]::SendWait("%-c")
+    Start-Sleep 5
+    [System.Windows.Forms.SendKeys]::SendWait("{UP}{UP}{UP}{UP}")
+    Start-Sleep 5
+    for ($i = 0; $i -le $indices[$indices.Count-1]; $i++) {
+        if($indices -contains $i) {
+            [System.Windows.Forms.SendKeys]::SendWait("~{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}~{DOWN}+{F10}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}~")
+            Start-Sleep 5
+            [System.Windows.Forms.SendKeys]::SendWait("~")
+            Start-Sleep 5
+            [System.Windows.Forms.SendKeys]::SendWait("~")
+            Start-Sleep 5
+            [System.Windows.Forms.SendKeys]::SendWait("~")
+            Start-Sleep 10
+            [System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}")
+        } else {
+            [System.Windows.Forms.SendKeys]::SendWait("{DOWN}")
+        }
+    }
 }
 
 # Import project with the gui wizard (because no shell interaction option is available)
@@ -45,4 +70,8 @@ foreach($project in $projects){
     [System.Windows.Forms.SendKeys]::SendWait($config)
     Start-Sleep 20
 }
+
+KeyStrokesForMetamodels $metamodelpositions
+
+
 Write-Host "Finished"
